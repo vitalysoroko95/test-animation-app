bc347c94c8cc17f3ed758268da66fd725c565320
Vitaly Soroko <v.soroko@tapston.com>
2023-02-20 20:14:19 +0300

    initial commit

---
+++ src/components/Block/Block.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Block = () => {
+  return <div className="block-container">Block</div>;
+};
+
+export default Block;
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+function Main() {
+  return <div className="main-container">Main</div>;
+}
+
+export default Main;
